81ebee5f5626fb931408516aef820d0d31bb0bc1
Diaz235701 <167369560+Diaz235701@users.noreply.github.com>
2024-04-18 11:33:39 -0500

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,25 @@
+a=int(input("Digite el valor para a:"))
+b=int(input("Digite el valor para b:"))
+resta=a-b
+print("la resta es:" ,resta)
+
+
+
+a=int(input("Digite el valor para a:"))
+b=int(input("Digite el valor para b:"))
+suma=a+b
+print("la suma  es:" ,suma)
+
+
+
+a=int(input("Digite el valor para a:"))
+b=int(input("Digite el valor para b:"))
+division=a/b
+print("la division es:" ,division)
+
+
+
+a=int(input("Digite el valor para a:"))
+b=int(input("Digite el valor para b:"))
+multiplicacion=a*b
+print("la multiplicacion es:" ,multiplicacion)
---
+++ ejercicio3.py
@@ -0,0 +1,4 @@
+a=int(input("digite el valor de la compra:"))
+b=int(input("digite el valor del billete:"))
+resta=a-b
+print("las devueltas son:" ,resta)
