0cfd20daf4660494c494c8f833b9101a860292a7
Diaz235701 <167369560+Diaz235701@users.noreply.github.com>
2024-04-23 14:18:35 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+#Declare variable
+nombre_del_estudiante = str(input("Ingrese el nombre de estudiante: "))
+nombre_colegio = str(input("Ingrese el nombre del colegio: "))
+grado = input("Ingrese el grado que cursa: ")
+
+
+print("El estudiante",nombre_del_estudiante,"Estudia en el colegio",nombre_colegio,"Grado en que cursa",grado)
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,25 +1,8 @@
-a=int(input("Digite el valor para a:"))
-b=int(input("Digite el valor para b:"))
-resta=a-b
-print("la resta es:" ,resta)
-
-
-
-a=int(input("Digite el valor para a:"))
-b=int(input("Digite el valor para b:"))
-suma=a+b
-print("la suma  es:" ,suma)
-
-
-
-a=int(input("Digite el valor para a:"))
-b=int(input("Digite el valor para b:"))
-division=a/b
-print("la division es:" ,division)
-
-
-
-a=int(input("Digite el valor para a:"))
-b=int(input("Digite el valor para b:"))
-multiplicacion=a*b
-print("la multiplicacion es:" ,multiplicacion)
+#Declare aqui las variables
+base = float(input("Ingrese la base del rectangulo: "))
+altura = float(input("Ingrese la altura del rectangulo: "))
+#datos 
+perimetro = 2 * (base + altura)
+area = base * altura
+#calculando el perimetro
+print("El prerimetro del rectangulo es:",perimetro,"y el area del rectangulo es:",area)
