1ac31416f62d1ba487e0d8b89d119472a73e8fec
Diaz235701 <167369560+Diaz235701@users.noreply.github.com>
2024-04-25 07:20:35 -0500

    Delete ejercicio6.py

--- ejercicio6.py
+++
@@ -1,9 +0,0 @@
-#declaramos variable
-nombre_estudiante = str(input("Ingrese el nombre del estudiante: "))
-calif_actividades = float(input("Ingrese la calificacion promedio de las actividades en clase: "))
-calif_proyecto = float(input("Ingrese la calificacion del proyecto final"))
-calif_evaluaciones = float(input("Ingrese la calificacion promedio de las evaluaciones"))
-#calculo de las notas final
-notas_final = (calif_actividades * 0.3) + (calif_proyecto * 0.5) + (calif_evaluaciones * 0.2)
-#mensaje
-print("La nota final de algoritmia del estudiante",nombre_estudiante,"es",notas_final)
